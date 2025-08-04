07dc2a5d900f78dd09302c8a8eaef88ea5d9c4fa
Edwin Odero <edwinodero70@gmail.com>
2025-08-04 14:36:40 +0300

    Update astro.config.mjs

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -7,6 +7,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	preview: {
+		port: 3000,
+		host: true,    // This enables listening on all network interfaces
+	},	
 	server:{
 		port: 3000,
 		host: true
